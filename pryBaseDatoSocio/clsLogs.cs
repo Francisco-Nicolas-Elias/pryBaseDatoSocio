823974403cafc43496ebe9ceefdbc7a802001476
root <mgiraudo@ies21.edu.ar>
2023-10-23 14:10:02 -0300

    clase Logs creada

---
+++ pryBaseDatoSocio/clsLogs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryBaseDatoSocio
+{
+    internal class clsLogs
+    {
+        public void logInicioSesion()
+        {
+            //Fecha 
+            //Hora
+            //Acción realizada
+            //Menú al que se accede
+        }
+    }
+}
